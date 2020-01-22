87c70ee441f74687decb38638e998e4a06e88ddd
ChristianDellarosa <Christiandellarosa98@gmail.com>
2020-01-22 15:40:22 -0300

    Add Interface INoticiaService and Interface IPeliculaService

--- project/src/main/java/com/web/appcine/controller/HomeController.java
+++ project/src/main/java/com/web/appcine/controller/HomeController.java
@@ -1,25 +1,25 @@
 package com.web.appcine.controller;
 
 import com.web.appcine.helpers.Utils;
-import com.web.appcine.model.Pelicula;
+import com.web.appcine.service.IPeliculasService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
 
 @Controller
 public class HomeController {
+
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+    @Autowired
+    private IPeliculasService peliculasService;
 
     @GetMapping(value = "/")
     public String home(Model model) {
         model.addAttribute("fechas", Utils.getNextDays(6));
-        model.addAttribute("peliculas",getPeliculas());
+        model.addAttribute("peliculas", peliculasService.searchAll());
         model.addAttribute("fechaBusqueda",simpleDateFormat.format(new Date()));
         return "index";
     }
@@ -29,35 +29,21 @@ public class HomeController {
         System.out.println("IdPelicula: "+ idPelicula);
         System.out.println("Fecha: "+ fecha);
 
-        model.addAttribute("peliculas",getPeliculas());
+        model.addAttribute("pelicula",peliculasService.searchById(idPelicula));
         return "detail";
     }
 
     @GetMapping(value="/movies")
     public String mostrarPeliculas(Model model) {
-        model.addAttribute("peliculas",getPeliculas());
+        model.addAttribute("peliculas", peliculasService.searchAll());
         return "movies";
-    }
-
-    private List<Pelicula> getPeliculas() {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-        List<Pelicula> peliculas = new LinkedList<>();
-        try {
-            peliculas.add(new Pelicula(0,"Power Rangers",120,"B","Aventura",formatter.parse("02-05-2017"),"PowerRangers.jpg",true));
-            peliculas.add(new Pelicula(1,"Cars 2",150,"A","Suspenso",formatter.parse("20-05-2017"),"Cars2.jpg",true));
-            peliculas.add(new Pelicula(2,"Contratiempo",80,"C","Accion",formatter.parse("28-05-2017"),"Contratiempo.jpg",false));
-            peliculas.add(new Pelicula(3,"TheWitcher",80,"A","Aventura",formatter.parse("28-05-2019"),"theWitcher.jpg",true));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return peliculas;
     }
 
     @PostMapping(value = "/search")
     public String buscarFecha(@RequestParam("fecha") String fecha, Model model) {
         System.out.println("Fecha: "+fecha);
         model.addAttribute("fechas", Utils.getNextDays(6));
-        model.addAttribute("peliculas",getPeliculas());
+        model.addAttribute("peliculas", peliculasService.searchAll());
         model.addAttribute("fechaBusqueda",fecha);
         return "index";
     }
---
+++ project/src/main/java/com/web/appcine/service/INoticiasService.java
@@ -0,0 +1,8 @@
+package com.web.appcine.service;
+
+import com.web.appcine.model.Noticia;
+
+public interface INoticiasService {
+
+    void save(Noticia noticia);
+}
