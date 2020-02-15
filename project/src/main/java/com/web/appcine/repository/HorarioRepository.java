b2168cdf7686b56c5f2e8e724052cb2b02d1e2f1
ChristianDellarosa <Christiandellarosa98@gmail.com>
2020-02-15 18:38:00 -0300

    Add Horario Repository

---
+++ project/src/main/java/com/web/appcine/repository/HorarioRepository.java
@@ -0,0 +1,7 @@
+package com.web.appcine.repository;
+
+import com.web.appcine.model.Horario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HorarioRepository extends JpaRepository<Horario,Integer> {
+}
