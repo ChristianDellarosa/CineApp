6e9119de8579578d9ef2f0ea25194785bcb802db
ChristianDellarosa <Christiandellarosa98@gmail.com>
2020-02-17 22:30:01 -0300

    Implement detallePelicula and Pelicula Service

---
+++ project/src/main/java/com/web/appcine/services/DetallePeliculaServiceJPA.java
@@ -0,0 +1,24 @@
+package com.web.appcine.services;
+
+import com.web.appcine.model.DetallePelicula;
+import com.web.appcine.repository.DetallePeliculaRepository;
+import com.web.appcine.services.interfaces.IDetallePeliculaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DetallePeliculaServiceJPA implements IDetallePeliculaService {
+
+    @Autowired
+    DetallePeliculaRepository detallePeliculaRepository;
+
+    @Override
+    public void insert(DetallePelicula detalle) {
+        detallePeliculaRepository.save(detalle);
+    }
+
+    @Override
+    public void delete(int idDetalle) {
+        detallePeliculaRepository.deleteById(idDetalle);
+    }
+}
---
+++ project/src/main/java/com/web/appcine/services/PeliculasServiceJPA.java
@@ -0,0 +1,69 @@
+package com.web.appcine.services;
+
+import com.web.appcine.model.Pelicula;
+import com.web.appcine.repository.PeliculaRepository;
+import com.web.appcine.services.interfaces.IPeliculasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PeliculasServiceJPA implements IPeliculasService {
+
+    @Autowired
+    private PeliculaRepository peliculaRepository;
+
+    @Override
+    public List<Pelicula> searchAll() {
+        return peliculaRepository.findAll();
+    }
+
+    @Override
+    public Pelicula searchById(int idPelicula) {
+        Optional<Pelicula> pelicula = peliculaRepository.findById(idPelicula);
+        if(pelicula.isPresent()){
+            return pelicula.get();
+        }
+    return null;
+    }
+
+    @Override
+    public void insert(Pelicula pelicula) {
+        peliculaRepository.save(pelicula);
+    }
+
+    @Override
+    public List<String> searchGenders() {
+        // Nota: Esta lista podria ser obtenida de una BD
+        List<String> generos = new ArrayList<>();
+        generos.add("Accion");
+        generos.add("Aventura");
+        generos.add("Accion y Aventura");
+        generos.add("Romantica");
+        generos.add("Infantil");
+        generos.add("Terror");
+        generos.add("Drama");
+        generos.add("Comedia Romantica");
+        generos.add("Prueba");
+        return generos;
+    }
+
+    @Override
+    public List<String> searchCategories() {
+        // Nota: Esta lista podria ser obtenida de una BD
+        List<String> categorias = new ArrayList<>();
+        categorias.add("A");
+        categorias.add("B");
+        categorias.add("C");
+        categorias.add("D");
+        return categorias;
+    }
+
+    @Override
+    public void delete(int idPelicula) {
+        peliculaRepository.deleteById(idPelicula);
+    }
+}
